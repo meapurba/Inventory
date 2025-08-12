a76db52ae4b3a96a4a10a25792b430b44841e5a7
Apurba Barai <meapurba5@gmail.com>
2025-08-12 19:23:43 +0600

    Dashboard UI added

---
+++ Inventory/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
